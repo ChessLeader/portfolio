e7d085f31091a4473580034a1ecd4c5f60f96e0a
Andrii Kalinowski <andrii.kalinowski@gmail.com>
2022-09-14 16:47:52 +0200

    first commit

---
+++ portfolio/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <my-header/>
+</template>
+
+<script>
+  import MyHeader from "@/components/Header";
+  export default {
+    name: 'app',
+    components: {MyHeader}
+  }
+</script>
+
+<style>
+
+</style>
